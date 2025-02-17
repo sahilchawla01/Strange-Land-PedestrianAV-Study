94a0daf6f51ca0dd90ec00d032e556e7358ea545
Sahil Chawla <sahilchawla2312@gmail.com>
2025-02-16 23:00:45 -0500

    fix: fixed forward transform of car

--- Assets/Scripts/Ped_AV_Study/ScriptableObjectBase/CarAnimationSetting.cs
+++ Assets/Scripts/Ped_AV_Study/ScriptableObjectBase/CarAnimationSetting.cs
@@ -1,12 +1,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "CarAnimationSetting", menuName = "Scriptable Objects/CarAnimationSetting")]
-public class CarAnimationSetting : ScriptableObject
+namespace Ped_AV_Study.ScriptableObjectBase
 {
-    // Distance from the final position to start the animation
-    public float startDistance = 100.0f;
-    // Total time for the animation
-    public float animationTime = 2.0f;
-    public List<CarAudioSetting> carAudioSettings = new List<CarAudioSetting>();
+    [CreateAssetMenu(fileName = "CarAnimationSetting", menuName = "Scriptable Objects/CarAnimationSetting")]
+    public class CarAnimationSetting : ScriptableObject
+    {
+        // Distance from the final position to start the animation
+        public float startDistance = 100.0f;
+        // Total time for the animation
+        public float animationTime = 2.0f;
+        public List<CarAudioSetting> carAudioSettings = new List<CarAudioSetting>();
+    }
 }
--- Assets/Scripts/Ped_AV_Study/ScriptableObjectBase/CarAudioSetting.cs
+++ Assets/Scripts/Ped_AV_Study/ScriptableObjectBase/CarAudioSetting.cs
@@ -1,10 +1,13 @@
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "CarAudioSetting", menuName = "Scriptable Objects/CarAudioSetting")]
-public class CarAudioSetting : ScriptableObject
+namespace Ped_AV_Study.ScriptableObjectBase
 {
-    public float volume = 1.0f;
-    //The time from the start of car animation to play the audio 
-    public float timeToPlayAudio = 1.0f;
-    public AudioClip audioClipToPlay;
+    [CreateAssetMenu(fileName = "CarAudioSetting", menuName = "Scriptable Objects/CarAudioSetting")]
+    public class CarAudioSetting : ScriptableObject
+    {
+        public float volume = 1.0f;
+        //The time from the start of car animation to play the audio 
+        public float timeToPlayAudio = 1.0f;
+        public AudioClip audioClipToPlay;
+    }
 }
