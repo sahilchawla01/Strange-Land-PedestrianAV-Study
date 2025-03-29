ca117dcce2388a3a43afcaea02789a0f199f1e60
Sahil Chawla <sahilchawla2312@gmail.com>
2025-03-29 17:30:28 -0400

    feat: dynamically added audio toggle options

--- Assets/Scripts/Ped_AV_Study/UI/ScenarioPickerManager.cs
+++ Assets/Scripts/Ped_AV_Study/UI/ScenarioPickerManager.cs
@@ -1,34 +1,53 @@
+using System;
+using System.Collections.Generic;
 using System.Linq;
 using Ped_AV_Study;
 using Ped_AV_Study.ScriptableObjectBase;
 using UnityEngine;
+using UnityEngine.Serialization;
 using UnityEngine.UI;
 
 public class ScenarioPickerManager : MonoBehaviour
 {
 
-    public GameObject MainMenuUI;
-    public GameObject CarBehaviorMenuUI;
-    public GameObject StoppingDistMenuUI;
-    public GameObject AudioMenuUI;
-    public GameObject SelectAudioMenuUI;
-    public GameObject CarAnimPlayingUI;
+    [Header("Menu UI Elements")]
+    public GameObject mainMenuUI;
+    public GameObject carBehaviorMenuUI;
+    public GameObject stoppingDistMenuUI;
+    public GameObject audioMenuUI;
+    public GameObject selectAudioMenuUI;
+    public GameObject carAnimPlayingUI;
     
+    [Header("UI Toggle Group Elements")]
     public ToggleGroup carBehaviorToggleGroup;
     public ToggleGroup stoppingDistanceToggleGroup;
+    public ToggleGroup audioToggleGroup;
 
+    [Header("Car Audio Menu Related References")]
+    public GameObject audioOptionsPanel;
+    public GameObject audioOptionPrefab;
+    public List<CarAudioSetting> carAudioSettings;
+    private Dictionary<int, CarAudioSetting> mapAudioUIToAudioSetting = new Dictionary<int, CarAudioSetting>();
+    [Header("Miscellaneous References")]
     public ManipulateCar manipulateCarScript;
-    private CarAnimationSetting currentCarAnimationSetting;
+    
+    private CarAnimationSetting m_currentCarAnimationSetting;
+    private CarAudioSetting m_currentCarAudioSetting;
+
+    void Start()
+    {
+        AddAudioClipsToMenu();
+    }
 
     // -- Car Animation Functions -- 
     public void StartCarAnimation()
     {
         HideAllMenus();
         
-        manipulateCarScript.SetAnimationSettings(currentCarAnimationSetting);
+        manipulateCarScript.SetAnimationSettings(m_currentCarAnimationSetting);
         manipulateCarScript.StartAnimation();
         
-        CarAnimPlayingUI.SetActive(true);
+        carAnimPlayingUI.SetActive(true);
     }
 
     public void RestartCarAnimation()
@@ -57,29 +76,75 @@ public class ScenarioPickerManager : MonoBehaviour
         
         if (toggle.gameObject.name == "StopToggle") //The car should stop
         {
-            currentCarAnimationSetting = Instantiate(manipulateCarScript.NearDistAnimationSetting);
+            m_currentCarAnimationSetting = Instantiate(manipulateCarScript.NearDistAnimationSetting);
         }
         else // The car should pass
         {
-            currentCarAnimationSetting = Instantiate(manipulateCarScript.NoStopAnimationSetting);
+            m_currentCarAnimationSetting = Instantiate(manipulateCarScript.NoStopAnimationSetting);
         }
         
         // Debug.Log("Saved Car Behavior " + currentCarAnimationSetting.name);
+
+        //Add audio settings in-case already selected and has been overwritten
+        CheckAndAddAudioSetting();
     }
 
     public void ReturnFromCarBehaviorMenu()
     {
         //Save car behavior in case it wasn't set
-        if(currentCarAnimationSetting == null)
+        if(m_currentCarAnimationSetting == null)
             SaveCarBehavior();
         
         ShowMainMenu();
     }
     
-    
     // --- END ---
     
     // --- Audio Menu Functions ---
+    
+    //Adds audio clip options to the audio menu based on the list of CarAudioSetting objects
+    private void AddAudioClipsToMenu()
+    {
+        //Iterate over CarAudioSetting list
+        foreach (CarAudioSetting carAudioSetting in carAudioSettings)
+        {
+            //Create audio prefab and attach it to audio options panel
+            GameObject audioOption = Instantiate(audioOptionPrefab, audioOptionsPanel.transform);
+
+            //Map ID of game object to car audio setting
+            mapAudioUIToAudioSetting.Add(audioOption.GetInstanceID(), carAudioSetting);
+        }
+    }
+    public void SaveAudioClipToPlay()
+    {
+        Toggle toggle = audioToggleGroup.ActiveToggles().FirstOrDefault();
+
+        if (toggle == null)
+        {
+            Debug.LogError("No audio clip toggle selected");
+            return;
+        }
+        
+        //Clear any audio setting on current car animation 
+        m_currentCarAnimationSetting.carAudioSettings.Clear();
+        
+        //Get audio setting corresponding to gameobject
+        CarAudioSetting carAudioSetting = mapAudioUIToAudioSetting[toggle.GetInstanceID()];
+
+        if (carAudioSetting == null)
+        {
+            Debug.LogWarning("No corresponding audio setting was found for audio option");
+            return;
+        }
+        
+        //Add audio setting to current animation setting
+        m_currentCarAnimationSetting.carAudioSettings.Add(carAudioSetting);
+        
+        //Store current car audio setting 
+        m_currentCarAudioSetting = carAudioSetting;
+        
+        Debug.Log("Added Car audio to current animation setting: " + carAudioSetting.name);
+    }
 
     public void ReturnFromSelectAudioMenu()
     {
@@ -104,25 +169,27 @@ public class ScenarioPickerManager : MonoBehaviour
         
         if (toggle.gameObject.name == "NearDistToggle") //The car should stop close to the ped
         {
-            currentCarAnimationSetting = Instantiate(manipulateCarScript.NearDistAnimationSetting);
+            m_currentCarAnimationSetting = Instantiate(manipulateCarScript.NearDistAnimationSetting);
         }
         else if(toggle.gameObject.name == "MediumDistToggle") // The car should stop at a medium distance
         {
-            currentCarAnimationSetting = Instantiate(manipulateCarScript.MediumDistAnimationSetting);
+            m_currentCarAnimationSetting = Instantiate(manipulateCarScript.MediumDistAnimationSetting);
         }
         else //The car should stop afar
         {
-            currentCarAnimationSetting = Instantiate(manipulateCarScript.FarDistAnimationSetting);
+            m_currentCarAnimationSetting = Instantiate(manipulateCarScript.FarDistAnimationSetting);
         }
         
         // Debug.Log("Saved Car Stopping Distance " + currentCarAnimationSetting.name);
 
+        //Add audio settings in-case already selected and has been overwritten
+        CheckAndAddAudioSetting();
     }
 
     public void ReturnFromStoppingDistMenu()
     {
         //Save car behavior in case it wasn't set
-        if(currentCarAnimationSetting == null)
+        if(m_currentCarAnimationSetting == null)
             SaveStoppingDistance();
         
         ShowMainMenu();
@@ -136,55 +203,55 @@ public class ScenarioPickerManager : MonoBehaviour
     {
         HideAllMenus();
         
-        CarBehaviorMenuUI.SetActive(true);
+        carBehaviorMenuUI.SetActive(true);
     }
     
     public void ShowStoppingDistMenu()
     {
         HideAllMenus();
         
-        StoppingDistMenuUI.SetActive(true);
+        stoppingDistMenuUI.SetActive(true);
     }
     
     public void ShowAudioMenu()
     {
         HideAllMenus();
         
-        AudioMenuUI.SetActive(true);
+        audioMenuUI.SetActive(true);
     }
 
     public void ShowSelectAudioMenu()
     {
         HideAllMenus();
         
-        SelectAudioMenuUI.SetActive(true);
+        selectAudioMenuUI.SetActive(true);
     }
 
     public void ShowMainMenu()
     {
         HideAllMenus();
         
-        MainMenuUI.SetActive(true);
+        mainMenuUI.SetActive(true);
     }
 
     // -- HIDE MENUS Function --
     private void HideCarBehaviorMenu()
     {
-        CarBehaviorMenuUI.SetActive(false);
+        carBehaviorMenuUI.SetActive(false);
     }
     private void HideStoppingDistMenu()
     {
-        StoppingDistMenuUI.SetActive(false);
+        stoppingDistMenuUI.SetActive(false);
     }
     
     private void HideAudioMenu()
     {
-        AudioMenuUI.SetActive(false);
+        audioMenuUI.SetActive(false);
     }
     
     private void HideMainMenu()
     {
-        MainMenuUI.SetActive(false);
+        mainMenuUI.SetActive(false);
     }
 
     private void HideAllMenus()
@@ -193,7 +260,26 @@ public class ScenarioPickerManager : MonoBehaviour
         HideAudioMenu();
         HideStoppingDistMenu();
         HideCarBehaviorMenu();
-        CarAnimPlayingUI.SetActive(false);
-        SelectAudioMenuUI.SetActive(false);
+        carAnimPlayingUI.SetActive(false);
+        selectAudioMenuUI.SetActive(false);
+    }
+    
+    // -- END --
+    
+    // -- HELPER FUNCTIONS
+
+    // This checks if an audio setting is selected, if so, add it to the car animation setting 
+    //Usually used when audio setting is already selected, and user changes the entire car anim setting 
+    private void CheckAndAddAudioSetting()
+    {
+
+        if (m_currentCarAudioSetting == null)
+            return;
+        
+        //Clear any previous audio settings 
+        m_currentCarAnimationSetting.carAudioSettings.Clear();
+        
+        //Add selected audio setting
+        m_currentCarAnimationSetting.carAudioSettings.Add(m_currentCarAudioSetting);
     }
 }
